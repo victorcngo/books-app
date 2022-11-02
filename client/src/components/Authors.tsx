8a10832c386367000ce1d106d9f45b7d489b6e8c
victorcngo <hello@victorcango.fr>
2022-11-02 16:30:13 +0100

    feat(client): update Authors

--- client/src/components/Authors.tsx
+++ client/src/components/Authors.tsx
@@ -5,18 +5,33 @@ import { useEffect } from 'react'
 
 export default function Authors() {
   /* API fetch */
-  const { data } = useQuery(GET_AUTHORS)
+  const { data, loading, error } = useQuery(GET_AUTHORS)
 
   useEffect(() => {
     console.log(data)
   }, [data])
+
+  if (loading) return <p>Chargement...</p>
+
+  if (error) return <p>Erreur (GET_BOOKS)</p>
+
+  interface Author {
+    author: string
+    id: number
+  }
 
   return (
     <section>
       <div>
         <h2>Authors</h2>
         <ul>
-          <li></li>
+          {data.authors.map((elem: Author, index: number) => {
+            return (
+              <li key={index}>
+                {elem.author} (id: {elem.id})
+              </li>
+            )
+          })}
         </ul>
       </div>
     </section>
