08ad1436ae42f9a058f13d853ed162a0c4119a0d
victorcngo <hello@victorcango.fr>
2022-11-03 23:34:27 +0100

    fix(App): useEffect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,10 +8,14 @@ import AuthorsPage from './routes/AuthorsPage'
 import { AppDispatch } from './store'
 import { useDispatch } from 'react-redux'
 import { fetchBooks } from './store/books/booksSlice'
+import { useEffect } from 'react'
 
 export default function App() {
   const dispatch = useDispatch<AppDispatch>()
-  dispatch(fetchBooks())
+
+  useEffect(() => {
+    dispatch(fetchBooks())
+  }, [])
 
   return (
     <div className="App">
